68a7a5aafc75813b204b789649a2b5e2b19971d3
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-02-25 11:24:09 -0600

    Corrected index types declaration.

---
+++ typings/index.d.ts
@@ -0,0 +1,12 @@
+import { cryptoReportersSettings, CryptoReporter } from './declarations';
+/**
+ * 
+ * 
+ * 
+ * CRYPTO REPORTER INTERFACE
+ * 
+ * 
+ * 
+ */
+declare function CryptoReporters(settings: cryptoReportersSettings): CryptoReporter;
+export = CryptoReporters;
